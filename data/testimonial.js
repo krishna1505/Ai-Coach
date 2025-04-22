64fcfb0cabd45b25e0e4bec2a64f2ff12b5d969a
krishna@123 <ky2288922@gmail.com>
2025-04-22 18:55:26 +0530

    preparing for deployment

---
+++ data/testimonial.js
@@ -0,0 +1,26 @@
+export const testimonial = [
+    {
+      quote:
+        "The AI-powered interview prep was a game-changer. Landed my dream job at a top tech company!",
+      author: "Sarah Chen",
+      image: "https://randomuser.me/api/portraits/women/75.jpg",
+      role: "Software Engineer",
+      company: "Tech India.",
+    },
+    {
+      quote:
+        "The industry insights helped me pivot my career successfully. The salary data was spot-on!",
+      author: "Michael Rodriguez",
+      image: "https://randomuser.me/api/portraits/men/75.jpg",
+      role: "Product Manager",
+      company: "StartUp Ind.",
+    },
+    {
+      quote:
+        "My resume's ATS score improved significantly. Got more interviews in two weeks than in six months!",
+      author: "Priya Patel",
+      image: "https://randomuser.me/api/portraits/women/74.jpg",
+      role: "Data engineer",
+      company: "Google",
+    },
+  ];
